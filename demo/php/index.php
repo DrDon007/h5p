16dd73838ea8b1059f997b98af1eea6401c6bc43
smohan16 <smohan027@gmail.com>
2021-05-24 12:15:25 +0530

    database added

--- demo/php/index.php
+++ demo/php/index.php
@@ -1,14 +1,23 @@
 <?php
-$result1 = $_COOKIE["answer1"];
+$result1 = $_COOKIE["answer1"]; //get cookies from browser 
 $result2 = $_COOKIE["answer2"];
 $result3 = $_COOKIE["answer3"];
-echo $result1;
 
-// $con = mysqli_connect("localhost","root","","school")
 
-// if(mysqli_connect_errno()){
-//     echo "Connection Fail".mysqli_connect_error();
-// }
 
-// $query = mysqli_query($con,INSERT INTO data(q1,q2,q3))values('$result1','$result2','$result3')
+$servername = "localhost";
+$username = "root";                   //database conections
+$password = "";
+$dbname = "school";
+$conn = new mysqli($servername,$username,$password,$dbname);
+if($conn-> connect_error){
+    die("connection failed: ".$conn->connect_error);
+}
+$s = "INSERT INTO data(q1,q2,q3) VALUES ('$result1','$result2','$result3')"; //insert query
+if($conn->query($s) === TRUE) {
+    echo "New record added successfully";
+}else{
+    echo "Error: " .$s . "<br>" . $conn->error;
+}
+$conn->close();
 ?>
